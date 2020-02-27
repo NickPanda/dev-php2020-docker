1c2d60d852554ab2cd29f63da487381ec892e910
NickPanda <nick@badasspanda.ru>
2020-02-27 23:22:15 +0300

    FormatListUpdReadme

--- lesson3-linux-bash/table_top_city.sh
+++ lesson3-linux-bash/table_top_city.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-sed '1d' table_city | cut -d ' ' -f3,5- | sort | uniq -c | sort -r | awk '{$1=$1};1' | cut -d ' ' -f2-3,4- | head -n3
+    sed '1d' table_city |
+    cut -d ' ' -f3,5- | sort | uniq -c | 
+    sort -r |
+    awk '{$1=$1};1' | 
+    head -n3
